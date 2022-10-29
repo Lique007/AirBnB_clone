2f5b5b3166c9239881eca710041ff6d146e1b137
GetieBalew24 <getiebalew2020@gmail.com>
2022-10-29 15:14:05 +0300

    update base model class

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,21 @@
 #!/usr/bin/python3
-
 from uuid import uuid4
 from datetime import datetime
-import models
-
-
-
 
 class BaseModel:
-
-    if len(kwargs) > 0:
-        
-        for key, value in kwargs.items():
-            if key in ["created_at", "updated_at"]:
-                setattr(self, key,
-                        datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-
-            elif key != "__class__":
-                setattr(self, key, value)
-
-    else:
+    def __init__(self):
         self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        models.storage.new(self)
-        models.storage.save()
+        self.created_at = self.updated_at = datetime.now()
 
-    def __str__(self):                  # method for basemodel 
-
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__))
+    def __str__(self):
+        return f'[{type(self).__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
         self.updated_at = datetime.now()
-        models.storage.save()
 
-    def to_dict(self):      # returns a dict containing all the values
-
-        dict_ = dict(self.__dict__)
-        dict_.update({"__class__": self.__class__.__name__,
-                     "created_at" : str(((self.created_at).isoformat())),
-                     "updated_at" : str(((self.updated_at).isoformat()))})
-        return dict_
+    def to_dict(self):
+        my_dict = self.__dict__.copy()
+        my_dict['__class__'] = type(self).__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        return my_dict
