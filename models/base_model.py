06f83e783e8b03d831080a97a5e74bfdbbdbc08a
GetieBalew24 <getiebalew2020@gmail.com>
2022-10-29 15:59:18 +0300

    Update base_model class

--- models/base_model.py
+++ models/base_model.py
@@ -3,9 +3,17 @@ from uuid import uuid4
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs == {}:
+            self.id = str(uuid4())
+            self.created_at = self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    pass
+                if key in ['created_at','updated_at']:
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                self.__dict__[key] = value
 
     def __str__(self):
         return f'[{type(self).__name__}] ({self.id}) {self.__dict__}'
